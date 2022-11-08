83aea8a0d8ae186b7dfb5a1bc512a0f1db0fec18
sabidalam <Shafkatsaad@gmail.com>
2022-11-08 16:06:15 +0600

    added-update-section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './About/About';
 import Banner from './Banner/Banner';
+import LatestUpdate from './LatestUpdate/LatestUpdate';
 
 
 const Home = () => {
@@ -8,7 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <About></About>
-
+            <LatestUpdate></LatestUpdate>
         </div>
 
     );
---
+++ src/Pages/Home/LatestUpdate/LatestUpdate.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import image1 from '../../../assets/images/zambia.jpg';
+import image2 from '../../../assets/images/meerkats.jpg';
+import image3 from '../../../assets/images/Africa.jpg';
+
+const LatestUpdate = () => {
+    return (
+        <div className='my-12'>
+            <h1 className="mb-3 text-2xl font-bold text-center text-orange-600">Latest Updates</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-6xl mx-auto mt-8'>
+                <div className="w-96 bg-base-100">
+                    <figure><img src={image1} alt="" className='w-11/12 ml-5' /></figure>
+                    <div className="mt-5 ml-5">
+                        <h2 className="text-2xl font-semibold">Visiting Zambia Forest</h2>
+                        <p className='mt-4'>Zambia was amazing, spent time in Liuwa <br /> Plain, Kafue, Kasanka and Lower Zambezi <br /> National Parks</p>
+                    </div>
+                </div>
+                <div className="w-96 bg-base-100">
+                    <figure><img src={image2} alt="Shoes" className='w-11/12 ml-5' /></figure>
+                    <div className="mt-4 ml-5">
+                        <h2 className="text-2xl font-semibold">Makgadikgadi Pans region</h2>
+                        <p className='mt-4'>These wild meerkats have been habituated to humans so it possible to get close to them. Sometimes they will use people as convenient lookout-posts!</p>
+                    </div>
+                </div>
+                <div className="w-96 bg-base-100">
+                    <figure><img src={image3} alt="Shoes" className='w-11/12 ml-5' /></figure>
+                    <div className="mt-4 ml-5">
+                        <h2 className="text-2xl font-semibold">Visiting African Forest</h2>
+                        <p className='mt-4'>Africa's forests are some of the natural wonders of the world. I'm constantly amazed by the unique forest ecosystems on the continent.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='text-center'>
+                <button className="btn btn-error mt-4 px-10">View All</button>
+            </div>
+        </div>
+    );
+};
+
+export default LatestUpdate;
